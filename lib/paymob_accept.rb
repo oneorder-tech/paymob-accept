9655cbb04b42e3c8a2bfde1d95908080492aca31
Hesham Ghaith <hesham_magdy97@hotmail.com>
2022-08-18 14:45:07 +0200

    initial commit :tada:

---
+++ lib/paymob_accept.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "paymob_accept/version"
+
+module PaymobAccept
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/paymob_accept_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PaymobAccept do
+  it "has a version number" do
+    expect(PaymobAccept::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

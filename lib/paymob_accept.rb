b94e73f8426851e09e5412de1aed4b9adec9c6ad
Hesham Ghaith <hesham_magdy97@hotmail.com>
2022-08-18 15:08:44 +0200

    feat: Allow user to config PaymobAccept using Configuration class

--- lib/paymob_accept.rb
+++ lib/paymob_accept.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
-require_relative "paymob_accept/version"
+require_relative 'paymob_accept/version'
 
+require 'paymob_accept/configuration'
 module PaymobAccept
   class Error < StandardError; end
+
+  class << self
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+end
   # Your code goes here...
 end

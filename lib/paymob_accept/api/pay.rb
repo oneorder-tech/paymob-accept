3480ca6bfa5b3a369ceb00bb04c68a51cbf3da47
Hesham Ghaith <hesham_magdy97@hotmail.com>
2022-10-09 12:09:17 +0200

    enhancement: Allow calling auth payment method with iframe

--- lib/paymob_accept/api/pay.rb
+++ lib/paymob_accept/api/pay.rb
@@ -30,9 +30,9 @@ module PaymobAccept
 
       private
 
-      def pay_auth(customer:, address:, amount_cents:, amount_currency:)
+      def pay_auth(customer:, address:, amount_cents:, amount_currency:, iframe_id: nil)
         generate_payment_intent(customer: customer, address: address, amount_cents: amount_cents, amount_currency: amount_currency,
-                                integration_id: auth_integration_id)
+                                integration_id: auth_integration_id, iframe_id: iframe_id)
       end
 
       # Return an iFrame URL if an iframe_id is provided. Otherwise, returns a payment token

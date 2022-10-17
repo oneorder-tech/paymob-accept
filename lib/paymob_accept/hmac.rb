4533ebe954958cc427e7fba4fb91a51b47075ee0
Hesham Magdy <hesham_magdy97@hotmail.com>
2022-10-17 13:36:40 +0200

    Fix HMAC token to handle credit card tokenization (#6)
    * bump version
    
    * bump version
    
    * Add Hmac for Tokens

--- lib/paymob_accept/hmac.rb
+++ lib/paymob_accept/hmac.rb
@@ -1,16 +1,30 @@
 module PaymobAccept
   module Hmac
-    FILTERED_KEYS = %w[amount_cents created_at currency error_occured has_parent_transaction id
+    FILTERED_TRANSACTION_KEYS = %w[amount_cents created_at currency error_occured has_parent_transaction id
                        integration_id is_3d_secure is_auth is_capture is_refunded is_standalone_payment
                        is_voided order.id owner
                        pending source_data.pan source_data.sub_type source_data.type success].freeze
 
-    class << self
-      def validate(paymob_response:, hmac_key: PaymobAccept.configuration.hmac_key)
-        raise ArgumentError, 'hmac_key is required' if hmac_key.nil?
+    FILTERED_TOKEN_KEYS = %w[card_subtype created_at email id masked_pan merchant_id order_id token].freeze
 
+    class << self
+
+      def validate(paymob_response:, hmac_key: PaymobAccept.configuration.hmac_key)
+        validate_transaction?(paymob_response: paymob_response, hmac_key: hmac_key) || validate_token?(paymob_response: paymob_response, hmac_key: hmac_key)
+      end
+
+      def validate_transaction?(paymob_response:, hmac_key: PaymobAccept.configuration.hmac_key)
         digest = OpenSSL::Digest.new('sha512')
-        concatenated_str = FILTERED_KEYS.map do |element|
+        concatenated_str = FILTERED_TRANSACTION_KEYS.map do |element|
+          paymob_response.dig('obj', *element.split('.'))
+        end.join
+        secure_hash = OpenSSL::HMAC.hexdigest(digest, hmac_key, concatenated_str)
+        secure_hash == paymob_response['hmac']
+      end
+
+      def validate_token?(paymob_response:, hmac_key: PaymobAccept.configuration.hmac_key)
+        digest = OpenSSL::Digest.new('sha512')
+        concatenated_str = FILTERED_TOKEN_KEYS.map do |element|
           paymob_response.dig('obj', *element.split('.'))
         end.join
         secure_hash = OpenSSL::HMAC.hexdigest(digest, hmac_key, concatenated_str)
